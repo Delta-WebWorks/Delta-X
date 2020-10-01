6222b131ce76d4dfa3d6e4f5618ac1b24f645e2e
Prajneya <prajneya.kumar@research.iiit.ac.in>
2020-10-02 01:33:18 +0530

    refactor: add routes template

--- delta-x/src/App.js
+++ delta-x/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import landing from './components/landing/landing';
+
+class App extends Component {
+
+  constructor() {
+      super();
+      this.state = { game_selected: null };
+  }
+
+  render(){
+    return(
+      //add your component to this by Route path = "/{component name}"
+      
+      <BrowserRouter>
+        <div>
+          <Switch>
+            <Route path="/" component={landing} exact/>
+          </Switch>
+        </div>
+      </BrowserRouter>
+
+
+    );
+  }
 }
 
 export default App;
---
+++ delta-x/src/components/landing/landing.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class landing extends Component {
+
+	componentDidMount(){
+		
+	}
+
+	render() {
+    return (
+    	<>
+    		<div>Hello</div>
+    	</>
+      )
+  }
+}
+
+export default landing;
